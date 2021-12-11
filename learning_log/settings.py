3c4316607c7c06c689e935852e46d23219b3ceda
bastianposey <bastian_posey2@baylor.edu>
2021-12-10 23:39:33 -0600

    Styling base, index, login, topic, and topics

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,8 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
     'MainApp',
     'users',
+    # Third Party Apps
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
